270b992e283f5c1bb7a4f41037b9e1fd9733f82d
BSChampion <breevaneerden@outlook.com>
2022-07-05 17:01:45 +1000

    api fix

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.listen(port, () => console.log(`listening on port ${port}`))
 app.use(express.json())
 app.use(cors())
 
+app.use('/login', (req, res) => {
+  res.send({
+    token: 'test123'
+  });
+});
+
 let children = []
 
 app.get('/children', (req, res) => {

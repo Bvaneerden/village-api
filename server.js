ab4a2d025fb18b74cce6a7a3392dbda324761b1c
BSChampion <breevaneerden@outlook.com>
2022-07-05 17:27:13 +1000

    Add login endpoint again

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.listen(port, () => console.log(`listening on port ${port}`))
 app.use(express.json())
 app.use(cors())
 
-// app.use('/login', (req, res) => {
-//   res.send({
-//     token: 'test123'
-//   });
-// });
+app.use('/login', (req, res) => {
+  res.send({
+    token: 'test123'
+  });
+});
 
 let children = []
 

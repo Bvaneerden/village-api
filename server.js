52b0d5739b3af6741dd6f6c2fd0cb8e0c51375a0
BSChampion <breevaneerden@outlook.com>
2022-07-05 17:15:40 +1000

    api

--- server.js
+++ server.js
@@ -51,11 +51,11 @@ app.get('/findmedications', (req, res) => {
   })
 })
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   const path = require('path')
   app.use(express.static(path.join(__dirname, 'build')));
 
   app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
-}
+// }
